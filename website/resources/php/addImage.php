6003a9a6c8a384ec422efd6a99c1cfaf7f2f4929
drman15 <dxr120830@utdallas.edu>
2015-04-18 18:21:33 -0500

    addImage updated

--- website/resources/php/addImage.php
+++ website/resources/php/addImage.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	</head>
-	<body>
-		<form action="addImage.php" method="post" enctype="multipart/form-data">
-			<input type="file" name="image"><input type="submit" name="submit" value="Upload">
-		</form>
+<?php
+	if(isset($_POST['submit']))
+	{
+		$con = mysqli_connect('localhost', 'root', '', 'realestate_db');
+		mysqli_select_db($con, "realestate_db");
 		
-	<?php
-		$pass = "root"; // <-- Toggle
-		//$pass = ""; // <-- Toggle
-		if(isset($_POST['submit']))
+		$imageName = mysqli_real_escape_string($con, $_FILES["image"]["name"]);
+		$imageData = mysqli_real_escape_string($con, file_get_contents($_FILES["image"]["tmp_name"]));
+		$imageType = mysqli_real_escape_string($con, $_FILES["image"]["type"]);
+		
+		if(substr($imageType,0,5) == "image")
 		{
-			$con = mysqli_connect('localhost', 'root', $pass, 'realestate_db');
-			mysqli_select_db($con, "realestate_db");
-			
-			$imageName = mysqli_real_escape_string($con, $_FILES["image"]["name"]);
-			$imageData = mysqli_real_escape_string($con, file_get_contents($_FILES["image"]["tmp_name"]));
-			$imageType = mysqli_real_escape_string($con, $_FILES["image"]["type"]);
-			
-			if(substr($imageType,0,5) == "image")
-			{
-				mysqli_query($con, "INSERT INTO picture VALUES(1, '{$imageData}', 1, true)");
-			}
+			$numRows = mysqli_num_rows(mysqli_query($con, "SELECT * FROM Picture WHERE pid = " . $_POST['pid']));
+			mysqli_query($con, "INSERT INTO picture VALUES({$numRows} + 1, '{$imageData}', " . $_POST['pid'] . ", false)");
+			header('Location: ' . $_SERVER['HTTP_REFERER']);
 		}
-	?>
-	</body>
-</html>
+	}
+?>
